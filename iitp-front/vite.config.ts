35e1feceafc5f049c14a17136b797805999a90ae
hskim <hskim@gaia3d.com>
2025-04-22 11:03:55 +0900

    - CI/CD 테스트

--- iitp-front/vite.config.ts
+++ iitp-front/vite.config.ts
@@ -1,24 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import cesium from 'vite-plugin-cesium'
 import path from 'path'
 
-export default defineConfig({
-    server: {
-        hmr: true,
-        watch: {
-            usePolling: true, // 파일 변경 감지 방식을 Polling으로 강제
+export default defineConfig(({ mode }) => {
+    // 해당 모드의 .env 파일 로드 (예: .env.development, .env.production 등)
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        server: {
+            hmr: true,
+            watch: {
+                usePolling: true,
+            },
         },
-    },
-    define: {
-        global: 'globalThis'
-    },
-    plugins: [react(), cesium()],
-    resolve: {
-        alias: {
-            '@primitives': path.resolve(__dirname, 'src/primitives'),
-            '@stores': path.resolve(__dirname, 'src/stores'),
-            '@schema': path.resolve(__dirname, 'src/schema'),
+        define: {
+            global: 'globalThis',
+            'process.env': env, // 이걸 추가하면 JS 코드에서 process.env.XXX 도 사용 가능
+        },
+        plugins: [react(), cesium()],
+        resolve: {
+            alias: {
+                '@primitives': path.resolve(__dirname, 'src/primitives'),
+                '@stores': path.resolve(__dirname, 'src/stores'),
+                '@schema': path.resolve(__dirname, 'src/schema'),
+            }
         }
-    }
-})
+    };
+});
