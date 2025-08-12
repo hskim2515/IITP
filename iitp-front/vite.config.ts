67ccaa1c0907c1b60096a15a1fab128d2fc3d2d8
sjmin-gaia3d <sjmin@gaia3d.com>
2025-08-12 09:55:08 +0900

    refactor: 타입스크립트 경로 추가(#19)

--- iitp-front/vite.config.ts
+++ iitp-front/vite.config.ts
@@ -21,6 +21,9 @@ export default defineConfig(({ mode }) => {
         resolve: {
             alias: {
                 '@primitives': path.resolve(__dirname, 'src/primitives'),
+                '@api': path.resolve(__dirname, 'src/api'),
+                '@config': path.resolve(__dirname, 'src/config'),
+                '@component': path.resolve(__dirname, 'src/component'),
                 '@stores': path.resolve(__dirname, 'src/stores'),
                 '@schema': path.resolve(__dirname, 'src/schema'),
                 '@utils': path.resolve(__dirname, 'src/utils'),
