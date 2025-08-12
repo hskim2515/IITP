05eac59e1d418b34451029e70403a862ee042bbc
sjmin-gaia3d <sjmin@gaia3d.com>
2025-08-12 09:55:53 +0900

    refactor: 미사용 파일 제거(#19)

--- iitp-front/src/type/OpenLayersKeyOptions.ts
+++
@@ -1,8 +0,0 @@
-// Cesium과 버튼 구성이 다름
-// Cesium: LEFT_CLICK = 2
-// OpenLayers: LEFT_CLICK = 0
-export enum OpenLayersScreenSpaceEventType {
-    LEFT_CLICK = 0,
-    MIDDLE_CLICK = 1,
-    RIGHT_CLICK = 2,
-}
